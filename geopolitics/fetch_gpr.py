3c8c11721d83b56d6045f5cea3bfdd01681063cc
Cris Giovanoni <cris.giovanoni@finra.org>
2025-03-24 14:40:48 -0500

    Update source url for gpr

--- geopolitics/fetch_gpr.py
+++ geopolitics/fetch_gpr.py
@@ -16,12 +16,12 @@ def download_gpr_data():
 
         # read and write file
         df = pd.read_excel(xls, sheet_name=sheet_name)
-        output_file = 'gpr_data.csv'
+        output_file = 'geopolitics/gpr_data.csv'
         df.to_csv(output_file, index=False)
         print(f"GPR data successfully saved to {output_file}")
     
     else:
-        print(f"Failed to download GPR data: Status code {response.status_code}")
+        print(f"Failed to download GPR data:{response.status_code}")
         exit(1)
 
 if __name__ == "__main__":
