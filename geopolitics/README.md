0eaa2b32f301592313b88de26ea9ad39a89ecd17
Cris Giovanoni <cris.giovanoni@finra.org>
2025-03-24 14:11:34 -0500

    Move geopolitics files to repo

---
+++ geopolitics/README.md
@@ -0,0 +1,2 @@
+# geopolitics
+Retrievs Geopolitical Risk (GPR) Index data by Dario Caldara and Matteo Iacoviello
---
+++ geopolitics/fetch_gpr.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+from io import BytesIO
+
+# URL of the GPR data file
+gpr_url = 'https://www.matteoiacoviello.com/gpr_files/data_gpr_export.xls'
+
+def download_gpr_data():
+    response = requests.get(gpr_url)
+    
+    if response.status_code == 200:
+        data = BytesIO(response.content)
+        xls = pd.ExcelFile(data)
+        print("Available sheets:", xls.sheet_names) # to help debug
+        sheet_name = xls.sheet_names[0]  # grab first sheet
+
+        # read and write file
+        df = pd.read_excel(xls, sheet_name=sheet_name)
+        output_file = 'gpr_data.csv'
+        df.to_csv(output_file, index=False)
+        print(f"GPR data successfully saved to {output_file}")
+    
+    else:
+        print(f"Failed to download GPR data: Status code {response.status_code}")
+        exit(1)
+
+if __name__ == "__main__":
+    download_gpr_data()
