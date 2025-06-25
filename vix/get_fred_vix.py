d68e00936f8c260db9397fdcc2f493e4febdd82d
Cris Giovanoni <cris.giovanoni@finra.org>
2025-06-24 22:56:14 -0500

    Update get_fred_vix.py

--- vix/get_fred_vix.py
+++ vix/get_fred_vix.py
@@ -8,8 +8,8 @@ def fetch_vix_data():
     df = pd.read_csv(url, parse_dates=['DATE'])
     df = df.dropna()[['DATE', 'VIXCLS']]
     df["KEY_RISK_INDICATOR_ID"] = kri_id
-    output_file = 'vix_index/vix_data.csv'
-    os.makedirs('vix_index', exist_ok=True)
+    output_file = 'vix/vix_data.csv'
+    os.makedirs('vix', exist_ok=True)
     df.to_csv(output_file, index=False)
     print(f"VIX data saved to {output_file}")
 
