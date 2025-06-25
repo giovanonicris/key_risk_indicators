00d41f1f75846e7539a118be0b5e752273482c7f
Cris Giovanoni <cris.giovanoni@finra.org>
2025-06-24 23:14:32 -0500

    Update automation scripts in batch

--- fred_capacity_utilization/get_cumfns.py
+++ fred_capacity_utilization/get_cumfns.py
@@ -11,9 +11,9 @@ os.makedirs("fred_supplier_deliveries_index", exist_ok=True)
 
 fred_api_key = os.getenv("FRED_API_KEY")
 fred = Fred(api_key=os.environ['FRED_API_KEY'])
-isrm_data = fred.get_series('CUMFNS')
-isrm_df = isrm_data.reset_index()
-isrm_df.columns = ['DATE', 'CUMFNS']
-isrm_df["KEY_RISK_INDICATOR_ID"] = kri_id
-isrm_df.to_csv("fred_capacity_utilization/cumfns.csv", index=False)
+cumfns_data = fred.get_series('CUMFNS')
+cumfns_df = cumfns_data.reset_index()
+cumfns_df.columns = ['DATE', 'CUMFNS']
+cumfns_df["KEY_RISK_INDICATOR_ID"] = kri_id
+cumfns_df.to_csv("fred_capacity_utilization/cumfns.csv", index=False)
 print("Updated cumfns.csv")
--- fred_t10y2y/get_fred_t10y2y.py
+++ fred_t10y2y/get_fred_t10y2y.py
@@ -7,13 +7,13 @@ kri_id = 104
 
 #set up
 load_dotenv()  # load variables from .env
-os.makedirs("fred_t10y2y_yield_curve", exist_ok=True)
+os.makedirs("fred_t10y2y", exist_ok=True)
 
 fred_api_key = os.getenv("FRED_API_KEY")
 fred = Fred(api_key=os.environ['FRED_API_KEY'])
-ovx_data = fred.get_series('T10Y2Y')
-ovx_df = ovx_data.reset_index()
-ovx_df.columns = ['DATE', 'T10Y2Y']
-ovx_df["KEY_RISK_INDICATOR_ID"] = kri_id
-ovx_df.to_csv("fred_t10y2y_yield_curve/yield_curve_spread.csv", index=False)
+t10y2y_data = fred.get_series('T10Y2Y')
+t10y2y_df = t10y2y_data.reset_index()
+t10y2y_df.columns = ['DATE', 'T10Y2Y']
+t10y2y_df["KEY_RISK_INDICATOR_ID"] = kri_id
+t10y2y_df.to_csv("fred_t10y2y/t10y2y.csv", index=False)
 print("Updated yield_curve_spread.csv")
