c197c621d6f4ffce462577e340e81cd72103f284
Cris Giovanoni <cris.giovanoni@finra.org>
2025-06-24 23:41:24 -0500

    Update VIX script

--- fred_vix/get_vix.py
+++ fred_vix/get_vix.py
@@ -6,15 +6,17 @@ url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?id=VIXCLS'
 
 def fetch_vix_data():
     df = pd.read_csv(url)
-    df = df[df['VIXCLS'] != '.']  # remove missing values
-    df['DATE'] = pd.to_datetime(df['DATE'])
-    df['VIXCLS'] = df['VIXCLS'].astype(float)
-    df = df[['DATE', 'VIXCLS']]
+    df.columns = [col.strip().upper() for col in df.columns]
+    df.rename(columns={"OBSERVATION_DATE": "DATE"}, inplace=True)
+    # drop missing values and filter columns
+    df = df.dropna(subset=["VIXCLS"])
+    df["DATE"] = pd.to_datetime(df["DATE"])
     df["KEY_RISK_INDICATOR_ID"] = kri_id
-    output_file = 'fred_vix/vix_data.csv'
+    df = df[["DATE", "VIXCLS", "KEY_RISK_INDICATOR_ID"]]
+    #prep to write
     os.makedirs('fred_vix', exist_ok=True)
-    df.to_csv(output_file, index=False)
-    print(f"VIX data saved to {output_file}")
+    df.to_csv('fred_vix/vix_data.csv', index=False)
+    print("VIX data saved to fred_vix/vix_data.csv")
 
 if __name__ == "__main__":
     fetch_vix_data()
