aa512dcf9f08aa27bcfcf094573576b73b6cd20c
Cris Giovanoni <cris.giovanoni@finra.org>
2025-06-25 23:49:28 -0500

    Update get_gpr.py

--- gpr/get_gpr.py
+++ gpr/get_gpr.py
@@ -20,8 +20,8 @@ def download_gpr_data():
         # read and write file
         df = pd.read_excel(xls, sheet_name=sheet_name)
         df.columns.values[0] = "Date" # rename 'month' for consistency across other data
-        df["KEY_RISK_INDICATOR_ID"] = kri_id
-        df = df.rename(columns={'Date':'DATE'})
+        df['KEY_RISK_INDICATOR_ID'] = kri_id
+        df = df[['Date', 'GPR', 'KEY_RISK_INDICATOR_ID']]
         output_file = 'gpr/gpr_data.csv'
         df.to_csv(output_file, index=False)
         print(f"GPR data successfully saved to {output_file}")
